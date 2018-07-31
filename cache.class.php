17ad291955ba3978948dd3c1a5721334d019d4cb
X-NicON <x-icon@ya.ru>
2018-07-31 14:23:58 +0300

    ref

--- cache.class.php
+++ cache.class.php
@@ -16,24 +16,24 @@ class Cache {
   /**
    * @var array
    */
-	private $cache = [];
-
-	/**
-	 * @var string
-	 */
-	private $dir;
+  private $cache = [];
 
   /**
    * @var string
    */
-	private $path;
+  private $dir;
 
-	function __construct($name = 'phpcache', $dir = null, $ext = '.cache') {
-		if($dir == null) {
-			$this->dir = sys_get_temp_dir();
-		} else {
-			$this->dir = $dir;
-		}
+  /**
+   * @var string
+   */
+  private $path;
+
+  function __construct($name = 'phpcache', $dir = null, $ext = '.cache') {
+    if($dir == null) {
+      $this->dir = sys_get_temp_dir();
+    } else {
+      $this->dir = $dir;
+    }
 
     if(!is_dir($this->dir) && !mkdir($this->dir, 0775, true)) {
       throw new Exception('Unable to create cache directory ('.$this->dir.')');
@@ -45,64 +45,64 @@ class Cache {
       }
     }
 
-		$this->path = $this->dir.'/'.$name.$ext;
+    $this->path = $this->dir.'/'.$name.$ext;
 
-		if(file_exists($this->path)) {
-			$file = file_get_contents($this->path);
-			if(!empty($file)) {
-				$this->cache = unserialize($file);
-			}
-		}
-	}
+    if(file_exists($this->path)) {
+      $file = file_get_contents($this->path);
+      if(!empty($file)) {
+        $this->cache = unserialize($file);
+      }
+    }
+  }
 
-	function __destruct() {
-		$this->savestore();
-	}
+  function __destruct() {
+    $this->savestore();
+  }
 
-	public function set($key, $value, $ttl = 0) {
-		if($ttl > 0)
-			$ttl += time();
+  public function set($key, $value, $ttl = 0) {
+    if($ttl > 0)
+      $ttl += time();
 
-		$this->cache[$key]['e'] = $ttl;
-		$this->cache[$key]['v'] = $value;
-	}
+    $this->cache[$key]['e'] = $ttl;
+    $this->cache[$key]['v'] = $value;
+  }
 
-	public function get($key) {
-		if($this->has($key)) {
-			return $this->cache[$key]['v'];
-		}
+  public function get($key) {
+    if($this->has($key)) {
+      return $this->cache[$key]['v'];
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	public function remove($key) {
-		if(array_key_exists($key, $this->cache)) {
-			unset($this->cache[$key]);
-			return true;
-		}
+  public function remove($key) {
+    if(array_key_exists($key, $this->cache)) {
+      unset($this->cache[$key]);
+      return true;
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	public function has($key) {
-		if(array_key_exists($key, $this->cache)) {
-			if($this->cache[$key]['e'] == 0 || $this->cache[$key]['e'] > time()) {
-				return true;
-			} else {
-				unset($this->cache[$key]);
-			}
-		}
+  public function has($key) {
+    if(array_key_exists($key, $this->cache)) {
+      if($this->cache[$key]['e'] == 0 || $this->cache[$key]['e'] > time()) {
+        return true;
+      } else {
+        unset($this->cache[$key]);
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	public function clean() {
-		$this->cache = [];
-		return $this->savestore();
-	}
+  public function clean() {
+    $this->cache = [];
+    return $this->savestore();
+  }
 
-	private function savestore() {
-		return (bool)file_put_contents($this->path, serialize($this->cache));
-	}
+  private function savestore() {
+    return (bool)file_put_contents($this->path, serialize($this->cache));
+  }
 
 }
